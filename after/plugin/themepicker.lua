aed44e22e54b3d8dd4b2e5cf54d79cc906f3d708
Niko256 <kolbnickolay@yandex.ru>
2025-04-20 22:07:27 +0300

    ..

--- after/plugin/themepicker.lua
+++ after/plugin/themepicker.lua
@@ -1,4 +1,4 @@
 require("themery").setup({
-  themes = {"poimandres", "blue-moon", "github_dark_tritanopia", "github_dark_high_contrast", "rose-pine",  "yorumi"}, 
+  themes = {"poimandres", "blue-moon", "github_dark_tritanopia", "github_dark_high_contrast", "rose-pine",  "yorumi", "embark"}, 
   livePreview = true, -- Apply theme while picking. Default to true.
 })
