fc45efdbdf247f70a49755c4dfc70922356ee48a
Niko256 <kolbnickolay@yandex.ru>
2025-09-12 17:54:02 +0300

    Tiny update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,187 +1,90 @@
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'rust_analyzer', 'clangd'},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({
-        capabilities = lsp_capabilities,
-      })
-    end,
-    lua_ls = function()
-      require('lspconfig').lua_ls.setup({
-        capabilities = lsp_capabilities,
-        settings = {
-          Lua = {
-            runtime = {
-              version = 'LuaJIT'
-            },
-            diagnostics = {
-              globals = {'vim'},
-            },
-            workspace = {
-              library = {
-                vim.env.VIMRUNTIME,
-              }
-            }
-          }
-        }
-      })
-    end,
-    rust_analyzer = function()
-      require('rust-tools').setup({
-        server = {
-          on_attach = function(client, bufnr)
-            -- Customize keybindings and other LSP behaviors here
-          end,
-          capabilities = lsp_capabilities,
-        },
-      })
-    end,
-  }
-})
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
-require('luasnip.loaders.from_vscode').lazy_load()
-
-cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-})
-
-require('nvim-treesitter.configs').setup({
-  ensure_installed = { "c", "cpp", "lua", "vim", "query", "rust" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-})
-
-
-require('dap').configurations.cpp = {
-  {
-    name = "Launch",
-    type = "lldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-  },
-}
-
 local on_attach = function(client, bufnr)
-  if client.server_capabilities.documentSymbolProvider then
-    require("nvim-navic").attach(client, bufnr)
-  end
+    if client.server_capabilities.documentSymbolProvider then
+        require("nvim-navic").attach(client, bufnr)
+    end
 end
 
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = { 'rust_analyzer', 'clangd', 'lua_ls' },
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({
+                capabilities = lsp_capabilities,
+                on_attach = on_attach,
+            })
+        end,
 
+        lua_ls = function()
+            require('lspconfig').lua_ls.setup({
+                capabilities = lsp_capabilities,
+                on_attach = on_attach,
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = 'LuaJIT'
+                        },
+                        diagnostics = {
+                            globals = { 'vim' },
+                        },
+                        workspace = {
+                            library = {
+                                vim.env.VIMRUNTIME,
+                            }
+                        }
+                    }
+                }
+            })
+        end,
 
-vim.fn.sign_define("DiagnosticSignError", {text = "", texthl = "DiagnosticError"})
-vim.fn.sign_define("DiagnosticSignWarn", {text = "", texthl = "DiagnosticWarn"})
-vim.fn.sign_define("DiagnosticSignInfo", {text = "", texthl = "DiagnosticInfo"})
-vim.fn.sign_define("DiagnosticSignHint", {text = "", texthl = "DiagnosticHint"})
+        rust_analyzer = function()
+            require('rust-tools').setup({
+                server = {
+                    on_attach = on_attach,
+                    capabilities = lsp_capabilities,
+                    settings = {
+                        ["rust-analyzer"] = {
+                            diagnostics = {
+                                enable = true,
+                                disabled = { "unresolved-proc-macro" },
+                                experimental = { enable = true },
+                            },
+                            inlayHints = {
+                                enable = true,
+                            },
+                        }
+                    }
+                },
+                tools = {
+                    hover_actions = { auto_focus = true },
+                    inlay_hints = { auto = true },
+                },
+            })
+        end,
 
-vim.diagnostic.config({
-  virtual_text = false, -- Disable inline diagnostics
-  signs = true, -- Keep gutter signs
-  underline = false, -- Disable underline
-  update_in_insert = false,
-  severity_sort = true,
-  float = {
-    border = "rounded",
-    source = "always",
-    format = function(diagnostic)
-      return string.format(
-        "%s (%s)",
-        diagnostic.message,
-        diagnostic.source or "LSP"
-      )
-    end,
-  }
-})
-
-local rust_opts = {
-  tools = {
-    hover_actions = { auto_focus = true },
-    inlay_hints = { auto = true },
-  },
-  server = {
-    settings = {
-      ["rust-analyzer"] = {
-        diagnostics = {
-          enable = true,
-          disabled = {"unresolved-proc-macro"},
-          experimental = { enable = true },
-        }
-      }
+        clangd = function()
+            require('lspconfig').clangd.setup({
+                capabilities = lsp_capabilities,
+                on_attach = on_attach,
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    "--header-insertion=never",
+                    "--fallback-style=llvm",
+                    "--enable-config",
+                },
+                settings = {
+                    clangd = {
+                        fallbackFlags = { "-std=c++23" },
+                        diagnostics = { enable = true }
+                    }
+                }
+            })
+        end,
     }
-  }
-}
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
-  callback = function(event)
-    local opts = {buffer = event.buf}
-
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = { spacing = 2, prefix = "●" },
-        signs = true,
-        underline = false,
-      }
-    )
-
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<leader>v', function()
-      vim.diagnostic.open_float({
-        focusable = false,
-        border = "rounded",
-        header = "",
-        padding = 0,
-      })
-    end, opts)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  end,
 })
 
-require('lspconfig').clangd.setup({
-  capabilities = lsp_capabilities,
-  cmd = {
-    "clangd",
-    "--background-index",
-    "--clang-tidy",
-    "--header-insertion=never",
-  },
-  settings = {
-    clangd = {
-      fallbackFlags = { "-std=c++20" },
-      diagnostics = { enable = true }
-    }
-  }
-})
+vim.keymap.set('n', '<leader>j', vim.lsp.buf.format, opts)
